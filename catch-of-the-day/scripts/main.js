c2038ddad904f09d39d9e3d0d86be699fa6f559d
Jamie Strachan <jamie.strachan@cbc.ca>
2016-04-12 14:52:15 -0400

    finished video 6

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,16 +1,66 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var App = React.createClass({
+	render: function () {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header tagline="Fresh Seafood Market" />
+				</div>
+				<Order />
+				<Inventory />
+			</div>
+		)
+	}
+});
+
+var Header = React.createClass({
+	render: function () {
+		console.log(this.props);
+		return (
+			<header className="top">
+				<h1>Catch
+					<span className="ofThe"><span className="of">of</span> <span className="the">the</span></span>
+					Day</h1>
+				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
+			</header>
+		)
+	}
+});
+
+var Order = React.createClass({
+	render: function () {
+		return (
+			<p>Order</p>
+		)
+	}
+});
+
+var Inventory = React.createClass({
+	render: function () {
+		return (
+			<p>Inventory</p>
+		)
+	}
+});
+
 /*
 	Store picker component
 	This will let us make <StorePicker />
 */
 var StorePicker = React.createClass({
 	render: function() {
+		var name="Jamie";
 		return (
-			<p>hello</p>
+			<form className="store-selector">
+				<h2>Please Enter a Store {name}</h2>
+				{/* comment */}
+				<input type="text" ref="storeId" required />
+				<input type="submit" />
+			</form>
 		)
 	}
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
