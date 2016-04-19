3cb93f2d883cc2b6b8ab8c1c50643a8954ecf985
Jamie Strachan <jamie.strachan@cbc.ca>
2016-04-19 13:44:36 -0400

    finished video 10

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -11,6 +11,17 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 var h = require('./helpers');
 
 var App = React.createClass({
+	getInitialState: function () {
+		return {
+			fishes: {},
+			order: {}
+		}
+	},
+	addFish: function (fish) {
+		var timestamp = (new Date()).getTime();
+		this.state.fishes['fish-' + timestamp] = fish;
+		this.setState({fishes: this.state.fishes});
+	},
 	render: function () {
 		return (
 			<div className="catch-of-the-day">
@@ -18,8 +29,40 @@ var App = React.createClass({
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
+		)
+	}
+});
+
+var AddFishForm = React.createClass({
+	createFish: function (event) {
+		event.preventDefault();
+
+		var fish = {
+			name: this.refs.name.value,
+			price: this.refs.price.value,
+			status: this.refs.status.value,
+			desc: this.refs.desc.value,
+			image: this.refs.image.value
+		};
+
+		this.props.addFish(fish);
+		this.refs.fishForm.reset();
+	},
+	render: function () {
+		return (
+			<form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+				<input type="text" ref="name" placeholder="Fish Name"/>
+				<input type="text" ref="price" placeholder="Fish Price" />
+				<select ref="status">
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea type="text" ref="desc" placeholder="Desc"></textarea>
+				<input type="text" ref="image" placeholder="URL to Image" />
+				<button type="submit">+ Add Item </button>
+			</form>
 		)
 	}
 });
@@ -49,7 +92,10 @@ var Order = React.createClass({
 var Inventory = React.createClass({
 	render: function () {
 		return (
-			<p>Inventory</p>
+			<div>
+				<h2>Inventory</h2>
+				<AddFishForm {...this.props} />
+			</div>
 		)
 	}
 });
